a33bbf0e418274ac485d64f18cddef3db1c97bc3
Sishir Sivakumar <mmrssishir0306@gmail.com>
2023-05-15 22:43:55 +0530

    Add files via upload

--- grav_force.cpp
+++ grav_force.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 vector<float> grav_force(float m1, float m2, float *r1, float *r2) {
     // returns the force vector on m1 due to m2
-    // r_com = 3D vector of the position of the center of mass of the system
     // r1 = 3D vector of the position of m1
     // r2 = 3D vector of the position of m2
     // r12 = r2 - r1
-    // r1 = r_com - (m2/(m1+m2))*r12
     // G = 6.67408e-11 m^3 kg^-1 s^-2
     float G = 6.67408e-11;
     float r12[3];
@@ -26,8 +24,20 @@ vector<float> grav_force(float m1, float m2, float *r1, float *r2) {
 }
 
 int main() {
-    float m1, m2, r1[3], r_com[3];
-    m1 = 1; m2 = 1; r_com[0] = 1; r_com[1] = 2; r_com[2] = 3; r1[0] = 0; r1[1] = 2; r1[2] = 0;
-    vector<float> F_vec = grav_force(m1, m2, r1, r_com);
+    // m1, m2 in kg
+    // r1, r2 in m
+    float m1, m2, r1[3], r2[3];
+    // input format:
+    // m1 m2
+    // r1_x r1_y r1_z
+    // r2_x r2_y r2_z
+    cin >> m1 >> m2;
+    for (int i = 0; i < 3; i++) {
+        cin >> r1[i];
+    }
+    for (int i = 0; i < 3; i++) {
+        cin >> r2[i];
+    }
+    vector<float> F_vec = grav_force(m1, m2, r1, r2);
     cout << "F = (" << F_vec[0] << ", " << F_vec[1] << ", " << F_vec[2] << ")" << endl;
 }
